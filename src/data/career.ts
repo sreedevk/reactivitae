f67e24183c90596605e082f38755af3e9abc9b8a
sreedevk <sreedev@icloud.com>
2025-07-27 10:29:07 +0000

    grammatical error correction

--- src/data/career.ts
+++ src/data/career.ts
@@ -1,51 +1,49 @@
 const careerData = [
   {
     "name": "HousingMan",
-    "location": "Bengaluru, IN.",
+    "location": "Bengaluru, IN",
     "summary": [
-      "Designed and developed a customized CRM application for the real estate sales industry, focusing on the lead lifecycle",
-      "Implemented algorithms to categorize lead quality based on various factors such as source, pipeline status, and sales representative feedback",
+      "Designed and developed a customized CRM application for the real estate sales industry, focusing on the lead lifecycle.",
+      "Implemented algorithms to categorize lead quality based on various factors such as source, pipeline status, and sales representative feedback.",
     ],
-    "stack": "Ruby/Rails, Postgresql, Javascript, PHP, AWS.",
+    "stack": "Ruby/Rails, PostgreSQL, Javascript, PHP, AWS",
     "start_date": "Feb, 2018",
     "end_date": "Feb, 2019",
     "designation": "Software Engineer",
   },
   {
     "name": "Riskcovry",
-    "location": "Bengaluru, IN.",
+    "location": "Bengaluru, IN",
     "summary": [
       "Worked with a team of engineers to develop software for insurance sales reporting and payment collection.",
-      "Designed and implemented business administration tools for analyzing sales data, tracking customer feedback, and monitoring cash flow",
+      "Designed and implemented business administration tools for analyzing sales data, tracking customer feedback, and monitoring cash flow.",
     ],
-    "stack": "Ruby/Rails, Postgresql, Javascript, Python.",
+    "stack": "Ruby/Rails, PostgreSQL, Javascript, Python",
     "start_date": "Feb, 2019",
     "end_date": "Jan, 2020",
     "designation": "Software Engineer",
   },
   {
     "name": "TarkaLabs",
-    "location": "New York, US.",
+    "location": "New York, US",
     "summary": [
-      "Worked as a consultant for a leading music distribution company, collaborating with the team to develop a comprehensive tax deduction and revenue reporting system for music distribution and publishing revenue",
-      "Published articles covering various topics such as digital signal processing, functional programming & low level programming"
+      "Worked as a consultant for a leading music distribution company, collaborating with the team to develop a comprehensive tax deduction and revenue reporting system for music distribution and publishing revenue.",
+      "Published articles covering various topics such as digital signal processing, functional programming and low-level programming."
     ],
-    "stack":
-      "Ruby/Rails, Elixir/Phoenix/Broadway, Redis, Postgresql, MariaDB, Javascript, Python, Docker, AWS",
+    "stack": "Ruby/Rails, Elixir/Phoenix/Broadway, Redis, PostgreSQL, MariaDB, Javascript, Python, Docker, AWS",
     "start_date": "Jan, 2020",
     "end_date": "Oct, 2023",
     "designation": "Senior Software Engineer",
   },
   {
-    "name": "Tunecore Inc.,",
-    "location": "New York, US.",
+    "name": "Tunecore Inc.",
+    "location": "New York, US",
     "summary": [
-      "Architected high throughput distributed data pipelines for processing music streaming and publishing revenue data for over millions of artists.",
+      "Architected high throughput distributed data pipelines for processing music streaming and publishing revenue data for millions of artists.",
       "Developed a comprehensive tax withholding and reporting system for music distribution revenue.",
       "Made use of formal methods to architect and engineer consistent algorithms for streaming fraud penalty escalation."
     ],
-    "stack":
-      "Ruby/Rails, Elixir/Phoenix/Broadway, Redis, Postgresql, MariaDB, Javascript, Python, Docker, AWS",
+    "stack": "Ruby/Rails, Elixir/Phoenix/Broadway, Redis, PostgreSQL, MariaDB, Javascript, Python, Docker, AWS",
     "start_date": "Oct, 2023",
     "end_date": "Present",
     "designation": "Senior Software Engineer",
